441e73804170ccb22422f1427b15f0e2512d83f4
Matt Lovan <magrelo404@gmail.com>
2020-05-28 13:11:44 -0600

    add mongo users

---
+++ mongo/createUsers.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo 'Creating application user and db'
+
+mongo ${APP_MONGO_DB} \
+        --host localhost \
+        --port ${MONGO_PORT} \
+        -u ${MONGO_ROOT_USER} \
+        -p ${MONGO_ROOT_PASS} \
+        --authenticationDatabase admin \
+        --eval "db.createUser({user: '${APP_MONGO_USER}', pwd: '${APP_MONGO_PASS}', roles:[{role:'dbOwner', db: '${APP_MONGO_DB}'}]});"
+
+echo 'Application user and db: created'
+
+# echo 'Creating staging user and db'
+
+# mongo ${STAGING_MONGO_DB} \
+#         --host localhost \
+#         --port ${MONGO_PORT} \
+#         -u ${MONGO_ROOT_USER} \
+#         -p ${MONGO_ROOT_PASS} \
+#         --authenticationDatabase admin \
+#         --eval "db.createUser({user: '${STAGING_MONGO_USER}', pwd: '${STAGING_MONGO_PASS}', roles:[{role:'dbOwner', db: '${STAGING_MONGO_DB}'}]});"
