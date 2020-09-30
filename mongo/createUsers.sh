36e88ef87401929b78b92f8e8e24362547f8a372
Matt Lovan <magrelo404@gmail.com>
2020-09-30 12:11:44 -0600

    add clubstack db

--- mongo/createUsers.sh
+++ mongo/createUsers.sh
@@ -12,6 +12,19 @@ mongo ${APP_MONGO_DB} \
 
 echo 'Application user and db: created'
 
+
+echo 'Creating clubstack user and db'
+
+mongo ${APP_MONGO_DB} \
+        --host localhost \
+        --port ${MONGO_PORT} \
+        -u ${MONGO_ROOT_USER} \
+        -p ${MONGO_ROOT_PASS} \
+        --authenticationDatabase admin \
+        --eval "db.createUser({user: '${APP_MONGO_USER}', pwd: '${APP_MONGO_PASS}', roles:[{role:'dbOwner', db: '${CLUBSTACK_MONGO_DB}'}]});"
+
+echo 'Application user and db: created'
+
 # echo 'Creating staging user and db'
 
 # mongo ${STAGING_MONGO_DB} \
